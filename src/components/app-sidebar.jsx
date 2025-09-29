32084ad816aae79d67bb79fee0d759fb70fd3ea5
warling <warlinglopez01@gmail.com>
2025-09-29 10:20:37 -0400

    fix: update sidebar branding and correct header title to Dashboard

--- src/components/app-sidebar.jsx
+++ src/components/app-sidebar.jsx
@@ -153,7 +153,7 @@ export function AppSidebar({
             <SidebarMenuButton asChild className="data-[slot=sidebar-menu-button]:!p-1.5">
               <a href="#">
                 <IconInnerShadowTop className="!size-5" />
-                <span className="text-base font-semibold">Acme Inc.</span>
+                <span className="text-base font-semibold">CVincer.</span>
               </a>
             </SidebarMenuButton>
           </SidebarMenuItem>
