0982c51622c66f7bf0f9075761e001b6cb99c088
warling <warlinglopez01@gmail.com>
2025-10-01 10:52:33 -0400

    feat: simplify login page by removing registration form and enhancing login functionality with email magic link

--- src/app/log/page.jsx
+++ src/app/log/page.jsx
@@ -1,22 +1,12 @@
-"use client"
 import { LoginForm } from "@/components/log/login/login-form"
-import { RegisterForm } from "@/components/log/logup/register-form"
-import { useEffect, useState } from "react";
-import { set } from "zod";
 
 export default function LoginPage() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  useEffect(() => {
-    const storedIsLogin = sessionStorage.getItem("isLogin");
-    setIsLogin(storedIsLogin);
-  }, []);
   
   return (
     <div
       className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="w-full max-w-sm">
-        {isLogin ? <LoginForm /> : <RegisterForm />}
+        <LoginForm />
       </div>
     </div>
   );
