ef48789c22a3d8413f27d223b3c38a80ce628dbc
warling <warlinglopez01@gmail.com>
2025-10-02 11:53:56 -0400

    fic

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -32,13 +32,13 @@ export function SiteHeader({ user }) {
     fetchCredits();
   }, [user]);
   return (
-    <header className="flex fixed justify-between w-[85%] h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
       <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
         <h1 className="text-base font-medium">Dashboard</h1>
         <div className="ml-auto flex items-center gap-2">
-          <Button variant="ghost" asChild size="sm" className="border rounded-2xl">
+          <Button variant="ghost" asChild size="sm" className="border fixed right-4 bg-background rounded-2xl">
             <a href="/dashboard/buy-credits">
               <Coins className="mr-2 size-4" />
               <span className="font-mono tabular-nums">{credits}</span>
