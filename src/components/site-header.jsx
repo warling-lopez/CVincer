d9f473d341e259a5efea1ac927d7dd1424eafdfc
warling-lopez <warlinglopez01@gmail.com>
2026-03-30 14:35:19 -0400

    fix the sider-bar to colo white in to left all wellll

--- src/components/site-header.jsx
+++ src/components/site-header.jsx
@@ -33,7 +33,7 @@ export function SiteHeader({ user }) {
   }, [user]);
   return (
     <header className="flex fixed  md:relative h-(--header-height) shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full  rounded-r-full md:border-0 border-1 py-0.5 items-center gap-1 px-4 lg:gap-2 lg:px-6">
+      <div className="flex w-full bg-background  rounded-r-full md:border-0 border-1 py-0.5 items-center gap-1 px-4 lg:gap-2 lg:px-6">
         <SidebarTrigger className="-ml-1" />
         <Separator orientation="vertical" className="mx-2 data-[orientation=vertical]:h-4" />
         <h1 className="text-base font-medium">Dashboard</h1>
